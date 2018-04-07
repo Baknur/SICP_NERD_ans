dc0e0815217c94dae4e68845dc6cf752048525c0
BahonREX <34392732+Baknur@users.noreply.github.com>
2018-04-07 16:33:52 +0600

    Add files via upload

---
+++ 1.1-q2.py
@@ -0,0 +1,3 @@
+from operator import *
+def bob(a,b,c):
+    return pow(add(max(a,b),max(b,c)),2)
---
+++ 1.1-q4.py
@@ -0,0 +1,13 @@
+def bob(n):
+    while n!=1:
+        if n%2==0:
+            print(n)
+            n=n/2
+        elif n%2!=0:
+            print(n)
+            n=n*3+1
+            
+            
+        
+        
+    
